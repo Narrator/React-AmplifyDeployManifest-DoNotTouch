cbcdc5a6a892a5fb9f3972523376f75c13822f74
Kaushik Subramaniam Gnanaskandan <kgnanask@amazon.com>
2023-10-24 09:06:48 -0700

    Fix routing rules

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# React ToDo App with Compute Backend
+# React ToDo App with Compute
 
 ## What is this repo?
 
-- Comprehensive React Todo SPA using React 18.x
-- Frontend app served from the `/frontend` URL path
-- Express backend app served from the `/backend` URL path
-- Backend meant to be deployed as compute
-- Deploy manifest found in `deploy-manifest.json`
-- There's a `postbuild` script that copies all built assets to the `.amplify-hosting` folder and conforms to the deployment specification.
+- Comprehensive React Todo SPA using React 18.x found in `./src`, deployed to S3 and served from the `/todo` URL path
+- Express server in `./compute`, deployed as compute app and served from the root `/` URL path
+- Deploy manifest found in `./deploy-manifest.json`
+- There's a `postbuild` script that copies all built assets to the `.amplify-hosting` folder and conforms the output to the deployment specification.
 - React app forked from [maciekt07/TodoApp](https://github.com/maciekt07/TodoApp)
--- compute/index.ts
+++ compute/index.ts
@@ -9,34 +9,39 @@ app.listen(port, () => {
 
 app.use(express.json());
 
+// Homepage
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.status(200).send("<h1>Welcome to my compute React app!</h1>");
+});
+
 // Middleware that returns JSON response
-app.get('/backend/json', (req: express.Request, res: express.Response) => {
+app.get('/json', (req: express.Request, res: express.Response) => {
   res.status(200).json({ message: 'Hello World!' });
 });
 
 // Middleware that returns HTML response
-app.get('/backend/html', (req: express.Request, res: express.Response) => {
+app.get('/html', (req: express.Request, res: express.Response) => {
   res.status(200).send('<h1>Hello World!</h1>');
 });
 
 // Middleware that returns text response
-app.get('/backend/text', (req: express.Request, res: express.Response) => {
+app.get('/text', (req: express.Request, res: express.Response) => {
   res.status(200).send('Hello World!');
 });
 
 // Middleware that returns status code 400
-app.get('/backend/status-400', (req: express.Request, res: express.Response) => {
+app.get('/status-400', (req: express.Request, res: express.Response) => {
   res.sendStatus(400);
 });
 
 // Middleware that sets custom headers
-app.get('/backend/header', (req: express.Request, res: express.Response) => {
+app.get('/header', (req: express.Request, res: express.Response) => {
   res.set('x-my-header', 'my-header');
   res.status(200).send('Check the response headers in the Network tab!');
 });
 
 // Middleware that sets cache-control header
-app.get('/backend/cache-control', (req: express.Request, res: express.Response) => {
+app.get('/cache-control', (req: express.Request, res: express.Response) => {
   res.set('Cache-Control', `public, max-age=0`);
   res.status(200).send('Hello World!');
 });
